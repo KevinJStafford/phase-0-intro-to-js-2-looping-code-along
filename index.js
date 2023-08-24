3111aab5a13f25237391fdc05a0548a428272b2b
Kevin <staffordkevinjames@gmail.com>
2023-08-24 09:36:13 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+let thankYou = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+      thankYou.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+      debugger;
+    };
+  
+    return thankYou;
+};
+
+function countDown(number) {
+    let i = number;
+    while ( i >= 0) {
+        console.log(i);
+        i--;
+    }
+
+    return i;
+};
